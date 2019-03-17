00b74db9ba3d53e2b132318ed235fc171b57bed4
Cy Edmonds <cy_m_e9@hotmail.com>
2019-03-17 19:33:55 -0400

    Completed Assignment
    Contains all changes made since last update. Finished PyBank and PyPoll.  Jupyter notebook files included.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,45 +1,73 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "#import dependencies\n",
-    "import os\n",
-    "import csv"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "#path to file\n"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.1"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+#  import dependencies
+import os
+import csv
+
+#  path to .csv file for input and .txt file for output
+bankdata = os.path.join("Resources/budget_data.csv")
+summdata = os.path.join("Summary/budget_summary.txt")
+
+#  define lists for gathering data
+date = []
+porl = []
+
+#  read csv and append lists
+with open(bankdata, 'r') as csvfile:
+    csvread = csv.reader(csvfile)
+    next(csvread, None)
+    for row in csvread:
+        date.append(row[0])
+        porl.append(int(row[1]))
+
+#  define variables to be used for output and print statement
+#  tried to put these in after creating the lists, but it caused problems for the way I wanted to code this, so I put them here.
+months = len(date)     #  Total number of months
+tot_porl = 0     #  Total profit or loss over entire period. Set to Zero at start, will be modified in loop.
+grinc_porl = porl[0]     #  Greatest increase in profit. Starting equal to first entry.
+grdec_porl = porl[0]     #  Greatest decrease in profit. Starting equal to first entry.
+#  will create variable for date of greatest profit and greatest loss during loop. adding them now makes this bothersome
+
+#  Loop through list of profit and loos to find values for variables
+#  I use "q" in this because other letters (i, j, etc.) reminded of VBA. I say "NO" to that! Also, the "q" stands out to me.
+for q in range(len(porl)):
+    if porl[q] >= grinc_porl:
+        grinc_porl = porl[q]
+        grinc_date = date[q]     #  variable for date of greatest profit
+    elif porl[q] <= grdec_porl:  #  I thought else would go here, but it didn't like that, something I'll have to look into.
+        grdec_porl = porl[q]
+        grdec_date = date[q]
+    tot_porl += porl[q]     #  I like "+=" (it made this code shorter) I also like the ""-=, *=, /="" operators, will use them in the future
+
+#  create and calculate the average profit or loss
+avg_porl = round(tot_porl / months, 2)     #  this also didn't like being at the top with the way I tried to write this code
+
+#  I used this line to test my variables and debug code, just copy and pasted variables in to check before moving on
+#  type(avg_porl), print(avg_porl)     # The variables I left plugged in are the last ones tested
+
+#  print results of loops and data to terminal, do some formatting
+print("------------------------------")
+print("->   Financial Analysis     <-")
+print("------------------------------")
+print("")
+print("Total Months: " + str(months))
+print("Total: ${:,}".format(tot_porl))
+print("Average Change: ${:,}".format(avg_porl))
+print("Greatest Increase in Profit:  " + (grinc_date) + " (${:,})".format(grinc_porl))
+print("Greatest Decrease in Profit:  " + (grdec_date) + " (${:,})".format(grdec_porl))
+print("")
+print("<End Summary>")
+
+#  write the output text file and populate with data
+with open(summdata, 'w',) as banksum:
+    banksum.writelines("------------------------------" + '\n')
+    banksum.writelines("->   Financial Analysis     <-" + '\n')
+    banksum.writelines("------------------------------" + '\n')
+    banksum.writelines("" + '\n')
+    banksum.writelines("Total Months: " + str(months) + '\n')
+    banksum.writelines("Total: ${:,}".format(tot_porl) + '\n')
+    banksum.writelines("Average Change: ${:,}".format(avg_porl) + '\n')
+    banksum.writelines("Greatest Increase in Profit:  " + (grinc_date) + " (${:,})".format(grinc_porl) + '\n')
+    banksum.writelines("Greatest Decrease in Profit:  " + (grdec_date) + " (${:,})".format(grdec_porl) + '\n')
+    banksum.writelines("" + '\n')
+    banksum.writelines("<End Summary>" + '\n')
+    banksum.close
+    
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,45 +1,74 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "#import dependencies\n",
-    "import os\n",
-    "import csv"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "#path to file\n"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.1"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+#  import dependencies
+import os
+import csv
+
+#  create path to input data
+polldata = os.path.join("Resources/election_data.csv")
+summpoll = os.path.join("Summary/election_results.txt")
+
+#  create a dictionary for storing election data
+election = {}
+
+#  define lists for gathering data
+candidate = []
+cand_vote = []
+percent = []
+winnerlst = []
+
+#  set initial total votes to zero
+total_votes = 0
+
+#  Read the csvfile, skipping header and parse data to the dictionary
+with open(polldata, 'r') as csvfile:
+    csvread = csv.reader(csvfile)
+    next(csvread, None)
+    for row in csvread:
+        if row[2] in election.keys():
+            election[row[2]] = election[row[2]]+1
+        else:
+            election[row[2]] = 1
+        total_votes += 1
+
+#  fill lists with data from dictionary keys
+for key, value in election.items():
+    candidate.append(key)
+    cand_vote.append(value)
+# print(election)     #  for checking and debugging, note: had to reset kernel when running from jupyter notebook
+
+#  calculates percent of total votes each candidate received
+for q in cand_vote:
+    percent.append(round(q/total_votes*100, 1))
+# percent     #  checking and debugging
+
+#  create tuple from lists
+cand_tpl = list(zip(candidate, cand_vote, percent))
+# cand_tpl     #  checking and debugging
+
+#  determine winner  (accounts for a tie)
+for name in cand_tpl:
+    if max(cand_vote) == name[1]:
+        winnerlst.append(name[0])
+        winner = winnerlst[0]     #  originally created this variable with others at top, moved here when code caused problems
+        if len(winnerlst) > 1:
+            for r in range(1, len(winnerlst)):
+                winner = winner + "," + winnerlst[r]
+# winner     #  checking and debugging, note: can check to see if above loop works by running multiple times in jupyter nb
+
+#  write data to output file 'election_results.txt'
+with open(summpoll, 'w') as tally:
+    tally.write("------------------------------" + '\n')
+    tally.write("->     Election Results     <-" + '\n')
+    tally.write("------------------------------" + '\n')
+    tally.write("Total Votes: " + str(total_votes) + '\n')
+    tally.write("------------------------------" + '\n')
+    for entry in cand_tpl:
+        tally.write(entry[0] + ": " + str(entry[2]) + "% (" + str(entry[1]) + ")" + '\n')
+    tally.write("------------------------------" + '\n')
+    tally.write("Winner: " + winner + '\n')
+    tally.write("------------------------------" + '\n')
+    tally.close
+
+    #  print 'election_results.txt' to terminal
+with open(summpoll, 'r') as terminaltally:
+    print(terminaltally.read())
+    
